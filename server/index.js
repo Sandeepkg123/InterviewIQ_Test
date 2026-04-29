49b9a7f1b6e5f4c024223d3e0f926f1e8c3cbe74
Sandeep K G <sandeepkg.tech@gmail.com>
2026-04-29 17:05:32 +0530

    feat: integrate authentication routes and middleware into Express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/connectDb.js';
+import cookieParser from 'cookie-parser';
+import authrouter from './routes/auth.route.js';
 
 dotenv.config();
 import cors from 'cors';
@@ -8,12 +10,15 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
-});
+app.use("/api/auth", authrouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
