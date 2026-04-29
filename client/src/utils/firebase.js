086a791d0e5f3b5ebc718996a78ce53a21740808
Sandeep K G <sandeepkg.tech@gmail.com>
2026-04-29 16:57:30 +0530

    feat: initialize Firebase configuration and Google authentication provider

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getauth(app);
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 export { auth, provider }
