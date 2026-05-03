a70aa1774da95766a913af421ad53bfec96c8005
Sandeep K G <sandeepkg.tech@gmail.com>
2026-05-03 07:57:39 +0530

    refactor(server): reduce jwt token expiration from 1h to 5m

--- server/config/token.js
+++ server/config/token.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const genToken = (userId) => {
     try {
-        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '5m' });
         return token;
     }
     catch (error) {
